5e0bf954200f39b8bcd9543f6bc7f3bb0bb4ce1f
Malte Fillmer <malte.fillmer@gmail.com>
2022-01-19 13:11:43 +0000

    test module imports

---
+++ primitives/prism.py
@@ -0,0 +1,12 @@
+
+from solid.objects import cube, rotate, translate
+from math import sqrt
+
+
+def prism(height=5, length=100):
+    c = cube((length, height, height))
+    c -= rotate((45, 0, 0))(cube((length, height**2, height)))
+    c = rotate((135, 0, 0))(c)
+    c = translate((0, sqrt(height**2*2), 0))(c)
+
+    return c
