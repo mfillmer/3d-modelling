32700d3f27b2ac4ff96ba3e73c7d9848d11044fa
Malte Fillmer <malte.fillmer@gmail.com>
2022-01-20 22:52:08 +0000

    add rounded corners and logo

--- smart_farming/starter_kit/__init__.py
+++ smart_farming/starter_kit/__init__.py
@@ -8,6 +8,19 @@ from starter_kit.box import box
 from starter_kit.grid import grid
 from starter_kit.logo import arc, logo, tube
 
+inner_corner = arc(angle=90, r=4, h=42, w=2)
+outer_corner = arc(angle=90, r=4, h=60, w=2)
+
+inner_corners = translate((6, 68, 0))(rotate((0, 0, 0))(inner_corner))\
+    + translate((6, 6, 0))(rotate((0, 0, 90))(inner_corner))\
+    + translate((138, 6, 0))(rotate((0, 0, 180))(inner_corner))\
+    + translate((138, 68, 0))(rotate((0, 0, 270))(inner_corner))
+
+outer_corners = translate((2, 72, 0))(rotate((0, 0, 0))(outer_corner))\
+    + translate((2, 2, 0))(rotate((0, 0, 90))(outer_corner))\
+    + translate((142, 2, 0))(rotate((0, 0, 180))(outer_corner))\
+    + translate((142, 72, 0))(rotate((0, 0, 270))(outer_corner))
+
 Einsatzschneide = forward(70)(
     rotate((0, 0, 270))(
         sum([forward(x)(blade(70, length=4)) for x in range(0, 140, 8)]
@@ -34,13 +47,20 @@ Aufsatzschneide = forward(70)(
     )
 )
 
+logo_vertical = translate((0, 15, 30))(rotate((0, 90, 0))(logo(30)))
+
 Untersatz = box(144, 74, 60, 4) \
-    + translate((0, 2, 2))(box(144, 70, 40, 4)) \
-    + translate((0, 4, 2))(box(144, 66, 40, 4)) \
-    - translate((0, 2, 58))(cube((144, 70, 2)))
+    + translate((2, 2, 2))(box(140, 70, 40, 4)) \
+    - translate((0, 2, 58))(cube((144, 70, 2)))\
+    - translate((0, 22, 7))(logo_vertical)\
+    - translate((143, 22, 7))(logo_vertical)
 
 
-Einlegeboden = grid()
+Untersatz = Untersatz\
+    + inner_corners\
+    - outer_corners
+
+Einlegeboden = grid(length=138)
 
 
 Logo = logo()
--- smart_farming/starter_kit/logo.py
+++ smart_farming/starter_kit/logo.py
@@ -2,11 +2,11 @@ from solid.objects import cylinder, cube, rotate, translate
 
 
 def tube(r=10, h=1, wall=1):
-    return cylinder(r=r, h=h) - cylinder(r=r-wall, h=h)
+    return cylinder(r=r, h=h, segments=30) - cylinder(r=r-wall, h=h, segments=30)
 
 
 def arc(angle=30, r=10, h=1, w=1, segment=5):
-    quarter = tube(r, h, w)*cube(r*2)
+    quarter = tube(r, h, w)*cube((r*2, r*2, h))
     unit = quarter*rotate((0, 0, 90-segment))(quarter)
     return sum([rotate((0, 0, x))(unit) for x in range(segment, angle+segment, segment)])
 
