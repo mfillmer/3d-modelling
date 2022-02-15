79deaacf94e47c0465cbc6a904264cc4ac5773f6
Malte Fillmer <malte.fillmer@gmail.com>
2022-02-15 09:43:31 +0000

    add cap

--- smart_farming/stackable_pipe.py
+++ smart_farming/stackable_pipe.py
@@ -37,6 +37,28 @@ def socket(r=12, w=2, d=None):
     return top
 
 
+def adapter(r=12, w=2, d=None):
+    """
+    adapter which mounts into socket. diamoter/radius needs to be the same as the ones specified for the socket. Height will result in 7w.
+    """
+    if d is not None:
+        r = d/2
+    bottom = ring(r1=r-w, h=w, w=w)\
+        + ring(r1=r-w*2, h=w*2, w=w)\
+        + ring(r1=r-w, r2=r-w*2, h=w, w=w, dz=w)\
+        + ring(r1=r-w*2, h=w, w=w, dz=w*2) \
+        + ring(r1=r-w*2, r2=r+w, h=w*4, w=w, dz=w*3)\
+        + ring(r1=r, dz=w*6, h=w)\
+        - ring(r1=r+w, h=w*3, w=w, dz=w*5)
+    slots = sum([rotate((0, 0, a))(slot(r1=r-w + 0.5, w=w+0.5, h=w*2, angle=40))
+                for a in [0, 120, 240]])
+    stopper = sum([rotate((0, 0, a))(translate((r-w*3, -w, 0))
+                  (cube((w*2, w, w*5)))) for a in [0, 120, 240]])
+    stopper_ring = stopper - ring(r1=r - w*3, r2=r-w, h=w*2, dz=w*3, w=w*5)
+
+    return bottom - slots + stopper_ring
+
+
 def pipe(r=12, w=2, h=44, d=None):
     if d is not None:
         r = d/2
